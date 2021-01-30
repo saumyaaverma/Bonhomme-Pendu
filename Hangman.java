8108c4a569cd1c412b9a71f39b04205b13312dd2
ameliestemarie <63207608+ameliestemarie@users.noreply.github.com>
2021-01-30 00:37:54 -0500

    Update Hangman.java
    Added the field wordFound which keeps track of what the user should see.

--- Hangman.java
+++ Hangman.java
@@ -1,15 +1,17 @@
 import java.util.ArrayList;
-public class Hangman {
+public class hangman {
 
     private String aWord;
-    final private int aMaxTries = 5;
+    final private int aMaxTries = 6;
     private int aAttemptedTries;
     private ArrayList<Character> aLettersTried;
+    private ArrayList<Character> aWordFound;
 
     public void newHangman(String word) {
         aAttemptedTries = 0;
         aWord = word;
         aLettersTried.clear();
+        aWordFound= new ArrayList<Character>(word.length());
     }
 
     private void tryLetter(Character c) {
@@ -17,14 +19,17 @@ public class Hangman {
         for (int i =0; i< aWord.length(); i++) {
             if (aWord.charAt(i) == c) {
                 //Letter found, add it to gui
+                aWordFound.set(i, c);
                 temp = false;
             }
         }
         if (temp) {
             aLettersTried.add(c);
             aAttemptedTries++;
+            if (aAttemptedTries >= aMaxTries) {
+                //Print the gui, quit the game, maybe print the answer
+            }
 
         }
     }
 }
-
