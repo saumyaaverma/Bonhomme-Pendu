26775b63dd8c1606dc71e2d93a1af516b7ca99d9
saumyaaverma <57843476+saumyaaverma@users.noreply.github.com>
2021-01-30 14:47:29 -0500

    main.java for single player version

--- main.java
+++ main.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 public class main {
     public static void main(String[] args) {
         int lines = 0;
+        /*file addresses*/
         ArrayList<String> filePaths = new ArrayList<String>();
         filePaths.add("/Users/apple/IdeaProjects/Bonhomme Pendu/src/food.txt");
         filePaths.add("/Users/apple/IdeaProjects/Bonhomme Pendu/src/cities.txt");
@@ -17,24 +18,51 @@ public class main {
         filePaths.add("/Users/apple/IdeaProjects/Bonhomme Pendu/src/music.txt");
         System.out.println("Enter the number corresponding to the theme for the game");
         System.out.println("0 : Food \n1 : Cities \n2 : Computer Science \n3 : Countries \n4 : Famous People \n5 : Music");
+
+        /*ask for the theme*/
         int index = - 1;
-        while(index < 0 || index > 5) {
-            System.out.println("Select the number");
-            Scanner input = new Scanner(System.in);
-            index = input.nextInt();
-        }
+            while (index < 0 || index > 5) {
+                System.out.println("Select the number");
+                Scanner input = new Scanner(System.in);
+                index = input.nextInt();
+            }
         try {
+            /*count the number of lines in the file selected*/
             File file = new File(filePaths.get(index));
             Scanner fileName = new Scanner(file);
             while(fileName.hasNextLine()) {
                 fileName.nextLine();
                 lines++;
             }
+            /*select a random line which corresponds to a random word*/
             Random random = new Random();
             int randomInt = random.nextInt(lines);
             String line = Files.readAllLines(Paths.get(filePaths.get(index))).get(randomInt);
-            System.out.println(line);
             fileName.close();
+            Hangman hangman = new Hangman(line);
+            System.out.println(line);
+            System.out.println("Enter your guess");
+
+            /*check if word is guessed*/
+            /*make Hangman and GUI objects*/
+            while(hangman.getAttemptedTries() < hangman.getMaxTries() && !(hangman.wordIsFound())) {
+                HangmanGUI.addLimb(hangman.getAttemptedTries());
+                HangmanGUI.initialPrint(hangman.getWordFound());
+                Scanner letter = new Scanner(System.in);
+                String str = letter.next();
+                char character = str.charAt(0);
+                hangman.tryLetter(character);
+            }
+            if(hangman.wordIsFound()){
+                HangmanGUI.addLimb(hangman.getAttemptedTries());
+                HangmanGUI.initialPrint(hangman.getWordFound());
+                System.out.println("You won!");
+            }
+            else{
+                HangmanGUI.addLimb(6);
+                HangmanGUI.initialPrint(hangman.getWordFound());
+                System.out.println("You lost! The word was " + line.toLowerCase());
+            }
         } catch (Exception e) {
             e.getStackTrace();
         }
