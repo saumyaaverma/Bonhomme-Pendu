da55fb52f5cbe068d0d3bdd3b3a252890b38ea4d
saumyaaverma <57843476+saumyaaverma@users.noreply.github.com>
2021-01-30 17:17:48 -0500

    Version update to handle single player and multiplayer games

--- main.java
+++ main.java
@@ -1,3 +1,4 @@
+import java.io.Console;
 import java.io.File;
 import java.nio.file.Paths;
 import java.util.Random;
@@ -7,66 +8,116 @@ import java.util.ArrayList;
 
 public class main {
     public static void main(String[] args) {
-        int lines = 0;
-        /*file addresses*/
-        ArrayList<String> filePaths = new ArrayList<String>();
-        filePaths.add("/Users/apple/IdeaProjects/Bonhomme Pendu/src/food.txt");
-        filePaths.add("/Users/apple/IdeaProjects/Bonhomme Pendu/src/cities.txt");
-        filePaths.add("/Users/apple/IdeaProjects/Bonhomme Pendu/src/compsci.txt");
-        filePaths.add("/Users/apple/IdeaProjects/Bonhomme Pendu/src/countries.txt");
-        filePaths.add("/Users/apple/IdeaProjects/Bonhomme Pendu/src/famouspeople.txt");
-        filePaths.add("/Users/apple/IdeaProjects/Bonhomme Pendu/src/music.txt");
-        System.out.println("Enter the number corresponding to the theme for the game");
-        System.out.println("0 : Food \n1 : Cities \n2 : Computer Science \n3 : Countries \n4 : Famous People \n5 : Music");
+        String line;
+        System.out.println("Choose game mode");
+        System.out.println("0: Single player");
+        System.out.println("1: Multiplayer");
+        int version = -1;
 
-        /*ask for the theme*/
-        int index = - 1;
+        while (version < 0 || version > 1) {
+            System.out.println("Select correct number");
+            Scanner type = new Scanner(System.in);
+            version = type.nextInt();
+        }
+        if (version == 1) {
+
+            Console console = System.console();
+            char[] hiddenWord = console.readPassword("Player 1 enter your word: ");
+            line = "";
+
+            for (int i = 0; i < hiddenWord.length; i++) {
+                line += hiddenWord[i];
+            }
+
+            int numberOfTries = gameHelper(line);
+
+            Console console1 = System.console();
+            char[] hiddenWord1 = console1.readPassword("Player 2 enter your word: ");
+            line = "";
+
+            for (int i = 0; i < hiddenWord1.length; i++) {
+                line += hiddenWord1[i];
+            }
+
+            int numberOfTries1 = gameHelper(line);
+
+            if( numberOfTries <  numberOfTries1){
+                System.out.println("Player 1 is the winner with only " + numberOfTries + " mistake(s)");
+            }
+            else if(numberOfTries >  numberOfTries1){
+                System.out.println("Player 2 is the winner with only " + numberOfTries1 + " mistake(s)");
+            }
+            else{
+                System.out.println("It's a tie since both players used " + numberOfTries1 + " mistake(s)");
+            }
+        } else {
+            int lines = 0;
+            /*file addresses*/
+            ArrayList<String> filePaths = new ArrayList<String>();
+            filePaths.add("/Users/apple/IdeaProjects/Bonhomme Pendu/src/food.txt");
+            filePaths.add("/Users/apple/IdeaProjects/Bonhomme Pendu/src/cities.txt");
+            filePaths.add("/Users/apple/IdeaProjects/Bonhomme Pendu/src/compsci.txt");
+            filePaths.add("/Users/apple/IdeaProjects/Bonhomme Pendu/src/countries.txt");
+            filePaths.add("/Users/apple/IdeaProjects/Bonhomme Pendu/src/famouspeople.txt");
+            filePaths.add("/Users/apple/IdeaProjects/Bonhomme Pendu/src/music.txt");
+            System.out.println("Enter the number corresponding to the theme for the game");
+            System.out.println("0 : Food \n1 : Cities \n2 : Computer Science \n3 : Countries \n4 : Famous People \n5 : Music");
+
+            /*ask for the theme*/
+            int index = -1;
             while (index < 0 || index > 5) {
                 System.out.println("Select the number");
                 Scanner input = new Scanner(System.in);
                 index = input.nextInt();
             }
-        try {
-            /*count the number of lines in the file selected*/
-            File file = new File(filePaths.get(index));
-            Scanner fileName = new Scanner(file);
-            while(fileName.hasNextLine()) {
-                fileName.nextLine();
-                lines++;
+            try {
+                /*count the number of lines in the file selected*/
+                File file = new File(filePaths.get(index));
+                Scanner fileName = new Scanner(file);
+                while (fileName.hasNextLine()) {
+                    fileName.nextLine();
+                    lines++;
+                }
+                /*select a random line which corresponds to a random word*/
+                Random random = new Random();
+                int randomInt = random.nextInt(lines);
+                line = Files.readAllLines(Paths.get(filePaths.get(index))).get(randomInt);
+                fileName.close();
+            } catch (Exception e) {
+                line = "";
+                e.getStackTrace();
             }
-            /*select a random line which corresponds to a random word*/
-            Random random = new Random();
-            int randomInt = random.nextInt(lines);
-            String line = Files.readAllLines(Paths.get(filePaths.get(index))).get(randomInt);
-            fileName.close();
-            Hangman hangman = new Hangman(line);
-            System.out.println(line);
-            System.out.println("Enter your guess");
-
-            /*check if word is guessed*/
-            /*make Hangman and GUI objects*/
-            while(hangman.getAttemptedTries() < hangman.getMaxTries() && !(hangman.wordIsFound())) {
-                HangmanGUI.addLimb(hangman.getAttemptedTries());
-                HangmanGUI.initialPrint(hangman.getWordFound());
-                Scanner letter = new Scanner(System.in);
-                String str = letter.next();
-                char character = str.charAt(0);
-                hangman.tryLetter(character);
-            }
-            if(hangman.wordIsFound()){
-                HangmanGUI.addLimb(hangman.getAttemptedTries());
-                HangmanGUI.initialPrint(hangman.getWordFound());
-                System.out.println("You won!");
-            }
-            else{
-                HangmanGUI.addLimb(6);
-                HangmanGUI.initialPrint(hangman.getWordFound());
-                System.out.println("You lost! The word was " + line.toLowerCase());
-            }
-        } catch (Exception e) {
-            e.getStackTrace();
+            gameHelper(line);
         }
+
+
+    }
+
+    public static int gameHelper(String line) {
+        Hangman hangman = new Hangman(line);
+        System.out.println("Enter your guess");
+
+        /*check if word is guessed*/
+        /*make Hangman and GUI objects*/
+        while (hangman.getAttemptedTries() < hangman.getMaxTries() && !(hangman.wordIsFound())) {
+            HangmanGUI.addLimb(hangman.getAttemptedTries());
+            HangmanGUI.initialPrint(hangman.getWordFound());
+            Scanner letter = new Scanner(System.in);
+            String str = letter.next();
+            char character = str.charAt(0);
+            hangman.tryLetter(character);
+        }
+        if (hangman.wordIsFound()) {
+            HangmanGUI.addLimb(hangman.getAttemptedTries());
+            HangmanGUI.initialPrint(hangman.getWordFound());
+            System.out.println("You guessed it!");
+        } else {
+            HangmanGUI.addLimb(6);
+            HangmanGUI.initialPrint(hangman.getWordFound());
+            System.out.println("You didn't guess it! The word was " + line.toLowerCase());
+
+        }
+        return hangman.getAttemptedTries();
     }
 }
-
 
