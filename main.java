d16f70773922278dab8cfdb019fe2dc320e1cb73
saumyaaverma <57843476+saumyaaverma@users.noreply.github.com>
2021-01-30 01:38:11 -0500

    Updated so that client can choose the theme

--- main.java
+++ main.java
@@ -3,12 +3,28 @@ import java.nio.file.Paths;
 import java.util.Random;
 import java.util.Scanner;
 import java.nio.file.Files;
+import java.util.ArrayList;
 
 public class main {
     public static void main(String[] args) {
         int lines = 0;
+        ArrayList<String> filePaths = new ArrayList<String>();
+        filePaths.add("/Users/apple/IdeaProjects/Bonhomme Pendu/src/food.txt");
+        filePaths.add("/Users/apple/IdeaProjects/Bonhomme Pendu/src/cities.txt");
+        filePaths.add("/Users/apple/IdeaProjects/Bonhomme Pendu/src/compsci.txt");
+        filePaths.add("/Users/apple/IdeaProjects/Bonhomme Pendu/src/countries.txt");
+        filePaths.add("/Users/apple/IdeaProjects/Bonhomme Pendu/src/famouspeople.txt");
+        filePaths.add("/Users/apple/IdeaProjects/Bonhomme Pendu/src/music.txt");
+        System.out.println("Enter the number corresponding to the theme for the game");
+        System.out.println("0 : Food \n1 : Cities \n2 : Computer Science \n3 : Countries \n4 : Famous People \n5 : Music");
+        int index = - 1;
+        while(index < 0 || index > 5) {
+            System.out.println("Select the number");
+            Scanner input = new Scanner(System.in);
+            index = input.nextInt();
+        }
         try {
-            File file = new File("/Users/apple/IdeaProjects/Bonhomme Pendu/src/food.txt");
+            File file = new File(filePaths.get(index));
             Scanner fileName = new Scanner(file);
             while(fileName.hasNextLine()) {
                 fileName.nextLine();
@@ -16,7 +32,7 @@ public class main {
             }
             Random random = new Random();
             int randomInt = random.nextInt(lines);
-            String line = Files.readAllLines(Paths.get("/Users/apple/IdeaProjects/Bonhomme Pendu/src/food.txt")).get(randomInt);
+            String line = Files.readAllLines(Paths.get(filePaths.get(index))).get(randomInt);
             System.out.println(line);
             fileName.close();
         } catch (Exception e) {
